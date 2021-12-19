774ec6a385ac7fe2008fb3979b8e956e524276a4
keikod1 <madragimova@icloud.com>
2021-12-19 22:54:11 +0600

    hero.applySuperAbility

--- homework№4/main.swift
+++ homework№4/main.swift
@@ -11,16 +11,8 @@ var heroes: [Hero] = [Magic(),Medic(),Warrior()]
 
 for hero in heroes {
      
-    if hero is Magic {
-        
-        print(dump(Magic().applySuperAbility(superAbilityType: "Light Attack")))
-    }
-    if hero is Medic{
-        print(dump(Medic().applySuperAbility(superAbilityType: "Enliven")))
-    }
-    if hero is Warrior{
-        print(dump(Warrior().applySuperAbility(superAbilityType: "Critical Damage")))
-    }
+    hero.applySuperAbility(superAbilityType: "light Attack")
+
 }
 
 
